a1fe52df9963d6a3b8b7ee9d84ce0ac66d10f598
SeanCena <seansng1@gmail.com>
2023-04-28 00:49:14 -0700

    Added project cards

--- src/App.js
+++ src/App.js
@@ -1,24 +1,79 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import {
+  ChakraProvider,
+  Box,
+  Heading,
+  Text,
+  Code,
+  Link,
+  Icon,
+  VStack,
+  Grid,
+  HStack,
+  SimpleGrid
+} from '@chakra-ui/react';
+import {
+  FaGithub,
+  FaLinkedin,
+  FaEnvelope,
+  FaReact,
+  FaPython
+} from 'react-icons/fa';
+import theme from './theme';
+import Quote from './Quote';
+import ProjectCard from './ProjectCard';
+import { ColorModeSwitcher } from './ColorModeSwitcher';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <ChakraProvider theme={theme}>
+      <Box textAlign="center" fontSize="xl">
+        <Grid minH="100vh" p={3}>
+          <ColorModeSwitcher justifySelf="flex-end" />
+          <VStack spacing={8}>
+            <Heading as='h1' size='4xl' color='purple.900'> { /* Replace this with an actual logo! */ }
+              Sean Sng
+            </Heading>
+            <HStack fontSize="3xl">
+              <Link href='https://github.com/SeanCena' isExternal><Icon _hover={{ color: 'purple.900' }} as={FaGithub} /></Link>
+              <Link href='https://www.linkedin.com/in/sean-sng/' isExternal><Icon _hover={{ color: 'purple.900' }} as={FaLinkedin} /></Link>
+              <Link href='mailto:seansng1@gmail.com' isExternal><Icon _hover={{ color: 'purple.900' }} as={FaEnvelope} /></Link>
+            </HStack>
+            <Text maxWidth='50%'>
+              Computer Engineering student at Cal Poly SLO. Art, music, and code.
+            </Text>
+          </VStack>
+          <SimpleGrid minChildWidth='300px' spacing='20px' mt={8}>
+            { /* Projects go here */ }
+            <ProjectCard
+              img='assets/placeholder.jpeg'
+              title='Yugioh Probability Calulator'
+              description={`A hypergeometric calculator for the card game Yugioh. Calculates the
+                            probability of a player drawing certain cards in a given deck. Made
+                            with React.`}
+              href=''
+              github=''
+              />
+            <ProjectCard
+              img='assets/placeholder.jpeg'
+              title='Mashup'
+              description={`A DJ app for parties that analyzes everyone's Spotify accounts to
+                            understand their music tastes, then plays music that matches everybody's
+                            preferences. Made with React and Python (Flask).`}
+              href=''
+              github=''
+              />
+            <ProjectCard title='Coming soon...' description='A secret project in the works' />
+            <ProjectCard title='Coming soon...' description='A secret project in the works' />
+            <ProjectCard title='Coming soon...' description='A secret project in the works' />
+          </SimpleGrid>
+          {/*
+          <VStack mt={8}><Quote /></VStack>
+          */}
+          <VStack mt={8}><Code>Made with Chakra UI. 2023</Code></VStack>
+        </Grid>
+      </Box>
+    </ChakraProvider>
   );
 }
 
---
+++ src/ProjectCard.js
@@ -0,0 +1,48 @@
+// Project card component
+
+import {
+    Stack,
+    Text,
+    Image,
+    Heading,
+    Card,
+    Link,
+    CardHeader,
+    CardBody,
+    CardFooter,
+    HStack,
+    Button
+} from '@chakra-ui/react';
+
+import { FaGithub } from 'react-icons/fa'
+
+
+
+function LinkGithub(url) {
+    //
+}
+
+function ProjectCard(props) {
+    return (
+        <Card>
+            <Link href={props.href} _hover={{ color: 'purple.900' }}>
+                <CardBody>
+                    <Stack>
+                        <Image src={process.env.PUBLIC_URL + (props.img === undefined ? 'assets/placeholder.jpeg' : props.img)} />
+                        <Heading fontSize="3xl">{props.title}</Heading>
+                        <Text align='left'>{props.description}</Text>
+                    </Stack>
+                </CardBody>
+            </Link>
+            <CardFooter align='left'>
+                {props.github === undefined ? undefined : 
+                    <Link href={props.github}>
+                        <Button leftIcon={<FaGithub />}>View Github repo</Button>
+                    </Link>
+                }
+            </CardFooter>
+        </Card>
+    );
+}
+
+export default ProjectCard;
